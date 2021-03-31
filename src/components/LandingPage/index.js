a4a334c227367d4a1761e07fb2251a546afcf48e
madrigal1 <kurupgokul11@gmail.com>
2021-03-31 22:39:29 +0530

    feat: setup

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.css'
+
+
+const LandingPage = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LandingPage;
+
+
+
---
+++ src/components/UpcomingEvents/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.css';
+
+
+const UpcomingEvents = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UpcomingEvents;
